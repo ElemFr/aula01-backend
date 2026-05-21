0319e4cc910b1a694a1f09e650a708a50440a548
ElemFr <fbeatris7@gmail.com>
2026-05-21 15:31:57 -0300

    enviando alteraçoes

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
-console.log("Servidor iniciado");
+
+const express = require('express');
+
+const app = express();
+    
+    app.get('/', (req, res) => {
+    res.send('Pagina Inicial com Express');
+});
+
+    app.get('/sobre', (req, res) => {
+    res.send('Pagina sobre com Express');
+});
+
+
+    app.get('/contato', (req, res) => {
+    res.send('Pagina contato com express');
+});
+
+
+    app.get('/produtos', (req, res) => {
+    res.send('Lista de produtos');
+});
+
+
+    app.get('/servicos', (req, res) => {
+    res.send('Lista de servicos');
+});
+
+
+app.listen(3000, () => {
+console.log('Servidor rodando em http://localhost:3000');
+});
