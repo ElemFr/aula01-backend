bab21e161775b5b0007ed2528d7d0046858aa6e6
ElemFr <fbeatris7@gmail.com>
2026-05-22 14:13:58 -0300

    Aula 2  Cria servidor com Express

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
     
     app.get('/', (req, res) => {
-    res.send('Pagina Inicial com Express');
+    res.send('Pagina Inicial ');
 });
 
     app.get('/sobre', (req, res) => {
